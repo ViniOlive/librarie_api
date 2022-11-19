aa57e481df4f179bd000e51be0f33ddb232f7dcb
ViniOlive <olivervini7141@gmail.com>
2022-11-19 00:02:06 -0300

    Alterações do projeto

--- Main_API.py
+++ Main_API.py
@@ -1,94 +1,80 @@
-# coding: Latin-1
-import codecs
-import json
-
+import pymongo
+from pymongo import MongoClient
 from flask import Flask, request, jsonify
-import pyodbc
-import pandas as pd
-from datetime import datetime
 
 app = Flask(__name__)
+user_db = "Dev"
+pass_db = "teste123"
+db_name = "Librarie"
 
 
 class DbConection:
-    def getDbDriver(self):
-        """
-        Obtem o driver correto instalado
-        """
-        drivers = [item for item in pyodbc.drivers()]
-        driver = drivers[-1]
-        return driver
+    @staticmethod
+    def connect():
+        connection_string = f"mongodb+srv://{user_db}:{pass_db}@stockcontroldev.v8gl9k6.mongodb.net/?retryWrites=true"
+        client = MongoClient(connection_string)
 
-    def connection(self):
-        """
-        Cria conexão com banco de dados Bizin
-        """
-        return pyodbc.connect(
-            driver=self.getDbDriver(),
-            server='(localdb)\Local',
-            database='master',
-            # uid='$user',
-            # pwd='$password'
-        )
+        return client[db_name]
 
 
-# db_return = pd.read_sql('SELECT * FROM Livros', DbConection().connection())
-# print(db_return)
+@app.route('/Books', methods=['GET'])
+def show_books():
+    db = DbConection().connect()
+    db_table = [item for item in db['book'].find()]
 
-@app.route('/Livros', methods=['GET'])
-def consulta_livros():
-    db_param = "EXEC API_Consulta_Livros_Geral"
-    db_return = pd.read_sql(db_param, DbConection().connection())
-    dict_return = {"Livros": []}
+    return jsonify(db_table)
 
-    for line in db_return.index:
-        dict_aux = {}
-        for key, value in db_return.items():
-            if key == 'Data_Recebimento':
-                dict_aux.update({key: trata_data(data=value[line])})
+
+@app.route('/Book', methods=['GET'])
+def show_book_id():
+    db = DbConection().connect()
+
+    if request.args.get('book_id'):
+        book_id = request.args.get('book_id')
+        db_table = [item for item in db['book'].find({"_id": int(book_id)})]
+        return jsonify(db_table)
+    else:
+        return "Preencha o parametro book_id"
+
+
+@app.route('/Book', methods=['PUT'])
+def update_info_book():
+    db = DbConection().connect()
+    db_table = db['book']
+    book_id = request.args.get('book_id')
+    dict_up = {}
+
+    if book_id:
+        for key, value in request.args.items():
+            if value and not key == 'book_id':
+                if value.replace('.', '').isnumeric():
+                    dict_up.update({key: float(value)})
+                else:
+                    dict_up.update({key: value})
+
+        db_table.update_one({"_id": int(book_id)}, {"$set": dict_up})
+        return "Update realizado"
+    else:
+        return "Preencha o parametro book_id"
+
+@app.route('/Book/Insert', methods=['POST'])
+def insert_book():
+    db = DbConection().connect()
+    db_table = db['book']
+    book_id = db_table.find_one(sort=[("_id", pymongo.DESCENDING)])["_id"] + 1
+    dict_in = {"_id": book_id}
+
+    for key, value in request.args.items():
+        if value:
+            if value.replace('.', '').isnumeric():
+                dict_in.update({key: float(value)})
             else:
-                dict_aux.update({key: value[line]})
-        dict_return['Livros'].append(dict_aux)
+                dict_in.update({key: value})
+        else:
+            return f"O campo {key} deve ser preenchido."
 
-    dc = json.dumps(dict_return, indent=4, default=str)
-
-    return dc
-
-
-def trata_data(data):
-    return datetime.strftime(data, "%d-%m-%y %H:%M")
+    db_table.insert_one(dict_in)
+    return f"Livro ID: {book_id} inserido com sucesso"
 
 
-@app.route('/Livros?<int:id_livro>', methods=['GET'])
-def consulta_livro_id(id_livro):
-    db_param = f"EXEC API_Consulta_Livro_ID {id_livro}"
-    db_return = pd.read_sql(db_param, DbConection().connection())
-    dict_aux = {}
-
-    for line in db_return.index:
-        for key, value in db_return.items():
-            if key == 'Data_Recebimento':
-                dict_aux.update({key: trata_data(data=value[line])})
-            else:
-                dict_aux.update({key: value[line]})
-
-    return json.dumps(dict_aux, indent=4, default=str)
-
-
-@app.route('/Livros/Insere', methods=['POST'])
-def insere_livro():
-    novo_livro = request.get_json()
-    print(novo_livro['Nome_Livro'])
-    if not novo_livro['Nome_Livro'] and novo_livro['Valor'] == :
-        db_param = f"EXEC API_Insere_Livro {novo_livro['Nome_Livro']}, {novo_livro['Valor']}"
-        db_return = pd.read_sql(db_param, DbConection().connection())
-
-        return db_return
-
-
-
-
-
-
-# consulta_livros()
 app.run(port=5000, host='localhost', debug=True)
